d045610f01b4f8f0099f4a825e3866e9c2d0995e
EstherNjuguna <esternjuguna@gmail.com>
2022-12-07 21:19:33 -0800

    done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,5 @@
-t_score(a_dictionary):
+#!/usr/bin/python3
+def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
     v = list(a_dictionary.values())
