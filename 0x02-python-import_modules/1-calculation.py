86f1478794c895435e387e360f4a63e1ffbe2f7e
EstherNjuguna <EstherNjuguna>
2023-08-16 22:51:53 -0700

    done

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,11 +3,8 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    sum_result = add(a, b)
-    sub_result = sub(a, b)
-    mul_result = mul(a, b)
-    div_result = div(a, b)
-    print("10 + 5 =", sum_result)
-    print("10 - 5 =", sub_result)
-    print("10 * 5 =", mul_result)
-    print("10 / 5 =", div_result)
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import sys
+if __name__ == '__main__':
+    from sys import argv
 
-if __name__ == "__main__":
-    arguments = sys.argv[1:]  # Exclude the script name itself
-    total = sum(map(int, arguments))
-    print(total)
+    sum = 0
+    for arg in argv[1:]:
+        sum += int(arg)
+
+    print(sum)
