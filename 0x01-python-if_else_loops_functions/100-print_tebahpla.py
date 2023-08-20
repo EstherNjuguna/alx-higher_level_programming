12ae16d5724b4af1154224fbc5baa2283f276302
EstherNjuguna <EstherNjuguna>
2023-08-20 00:06:57 -0700

    done

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for i in range(90, 64, -1):
+    print("{:c}".format(i), end="")
+    if i != 65:
+        print(", ", end="")
---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * insert_node - Inserts a number into a sorted singly linked list.
+ * @head: A pointer to a pointer to the head of the list.
+ * @number: The number to be inserted.
+ *
+ * Return: The address of the new node, or NULL if it failed.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *new_node = NULL, *current = NULL, *prev = NULL;
+
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->n = number;
+    new_node->next = NULL;
+
+    if (*head == NULL || (*head)->n >= number)
+    {
+        new_node->next = *head;
+        *head = new_node;
+        return (new_node);
+    }
+
+    current = *head;
+    while (current != NULL && current->n < number)
+    {
+        prev = current;
+        current = current->next;
+    }
+
+    new_node->next = current;
+    prev->next = new_node;
+
+    return (new_node);
+}
