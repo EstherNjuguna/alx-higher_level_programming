c8cc8a040a55e427e31641ac508477d6d6a723c3
EstherNjuguna <EstherNjuguna>
2023-08-20 00:13:11 -0700

    done@

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(90, 64, -1):
-    print("{:c}".format(i), end="")
-    if i != 65:
-        print(", ", end="")
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
