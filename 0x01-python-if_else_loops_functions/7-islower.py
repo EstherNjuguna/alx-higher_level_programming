e6c2ebfdafe8224c38aaf32152350b3a9d6264be
EstherNjuguna <EstherNjuguna>
2023-08-19 23:01:44 -0700

    if else

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/bin/bash
+def islower(str):
+    for i in range(97,123):
+        if chr(i) == str:
+            return (True)
+        else:
+            False
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/bin/bash
+def uppercase(str):
+    """Print a string in uppercase."""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
